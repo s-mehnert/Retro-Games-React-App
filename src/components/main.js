240bdde98aac0776fb09a793e243c8581f712f7f
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-08-27 18:09:14 +0200

    add links to thumbnails

--- src/components/main.js
+++ src/components/main.js
@@ -3,28 +3,37 @@ import frog from '../images/frog.png';
 import connectFour from '../images/connectFour.JPG';
 
 const thumbnails = {
-    frogger : {
-        src : frog,
-        alt : 'Frogger'
-    },
-    connectFour : {
-        src : connectFour,
-        alt : 'Connect Four'
-    }
+  frogger : {
+    href : "https://s-mehnert.github.io/frogger_game/",
+    src : frog,
+    alt : 'Frogger'
+  },
+  connectFour : {
+    href : "https://s-mehnert.github.io/connectFour_game/",
+    src : connectFour,
+    alt : 'Connect Four'
+  }
 };
 
 function main() {
-    
+  
+  // function handleClick() {
+  //   alert('Successfully clicked --- placeholder text');
+  // }
+  // need to include the following attribute in img: onClick={handleClick}
+
   let figures = [];
   for (const game in thumbnails) {
-    figures.push({id: game, src: thumbnails[game].src, alt: thumbnails[game].alt})
+    figures.push({id: game, src: thumbnails[game].src, alt: thumbnails[game].alt, href: thumbnails[game].href});
   }
 
   figures = figures.map(game => (
     <figure>
+      <a href={game.href} target="_blank">
         <img src={game.src} alt={game.alt} title={game.alt} />
-        <figcaption>{game.alt}</figcaption>
-      </figure>
+      </a>
+      <figcaption>{game.alt}</figcaption>   
+    </figure>
   ));
 
   return (
