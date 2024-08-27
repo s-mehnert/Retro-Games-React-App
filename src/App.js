968a43b6a93a07f0f593b183d12093881d401bcc
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-08-27 15:58:46 +0200

    add test thumbnails for main.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import testComponent from './components/testComponent';
 import main from './components/main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">    
         {main()}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Attribution
-        </a>
       </header>
       <body>
         
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import frog from '../images/frog.png';
+import connectFour from '../images/connectFour.JPG';
+
+const thumbnails = {
+    frogger : {
+        src : frog,
+        alt : 'Frogger'
+    },
+    connectFour : {
+        src : connectFour,
+        alt : 'Connect Four'
+    }
+};
 
 function main() {
+    
+  let figures = [];
+  for (const game in thumbnails) {
+    figures.push({id: game, src: thumbnails[game].src, alt: thumbnails[game].alt})
+  }
+
+  figures = figures.map(game => (
+    <figure>
+        <img src={game.src} alt={game.alt} title={game.alt} />
+        <figcaption>{game.alt}</figcaption>
+      </figure>
+  ));
+
   return (
     <div>
       <h1>RETRO GAMES</h1>
       <p>Pick a game and play.</p>
+      <div class="games">
+        {figures}
+      </div>
     </div>
   );
 }
