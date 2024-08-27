25374cb47da3a0cfb6b64f700007f4e24a2594a9
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-08-27 22:52:48 +0200

    add thumbnails for all seven retro games

--- src/components/main.js
+++ src/components/main.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import frog from '../images/frog.png';
+import memory from '../images/memory.JPG';
+import mole from '../images/mole.png';
 import connectFour from '../images/connectFour.JPG';
+import spaceInvaders from '../images/spaceInvaders.JPG';
+import snake from '../images/snake.JPG';
+import frog from '../images/frog.png';
+import tetris from '../images/tetris.JPG';
 
-const thumbnails = {
-  frogger : {
-    href : "https://s-mehnert.github.io/frogger_game/",
-    src : frog,
-    alt : 'Frogger'
+const thumbnails = {  
+  memory : {
+    href : "https://s-mehnert.github.io/memory_game/",
+    src : memory,
+    alt : "Memory"
+  },
+  whackTheMole : {
+    href : "https://s-mehnert.github.io/whackTheMole_game/",
+    src : mole,
+    alt : "Whack The Mole"
   },
   connectFour : {
     href : "https://s-mehnert.github.io/connectFour_game/",
     src : connectFour,
     alt : 'Connect Four'
+  },
+  spaceInvaders : {
+    href : "https://s-mehnert.github.io/spaceInvaders_game/",
+    src : spaceInvaders,
+    alt : "Space Invaders"
+  },
+  snake : {
+    href : "https://s-mehnert.github.io/snake_game/",
+    src : snake,
+    alt : "Snake"
+  },
+  frogger : {
+    href : "https://s-mehnert.github.io/frogger_game/",
+    src : frog,
+    alt : 'Frogger'
+  },
+  tetris : {
+    href : "https://s-mehnert.github.io/tetris_game/",
+    src : tetris,
+    alt : "Tetris"
   }
 };
 
