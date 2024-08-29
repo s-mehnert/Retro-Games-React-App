be822fe3a80254ae4415eb545b16498ee31c8742
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-08-29 14:15:07 +0200

    put game data in separate file gameData.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,49 +1,5 @@
 import React from 'react';
-import memory from '../images/memory.JPG';
-import mole from '../images/mole.png';
-import connectFour from '../images/connectFour.JPG';
-import spaceInvaders from '../images/spaceInvaders.JPG';
-import snake from '../images/snake.JPG';
-import frog from '../images/frog.png';
-import tetris from '../images/tetris.JPG';
-
-const thumbnails = {  
-  memory : {
-    href : "https://s-mehnert.github.io/memory_game/",
-    src : memory,
-    alt : "Memory"
-  },
-  whackTheMole : {
-    href : "https://s-mehnert.github.io/whackTheMole_game/",
-    src : mole,
-    alt : "Whack The Mole"
-  },
-  connectFour : {
-    href : "https://s-mehnert.github.io/connectFour_game/",
-    src : connectFour,
-    alt : 'Connect Four'
-  },
-  spaceInvaders : {
-    href : "https://s-mehnert.github.io/spaceInvaders_game/",
-    src : spaceInvaders,
-    alt : "Space Invaders"
-  },
-  snake : {
-    href : "https://s-mehnert.github.io/snake_game/",
-    src : snake,
-    alt : "Snake"
-  },
-  frogger : {
-    href : "https://s-mehnert.github.io/frogger_game/",
-    src : frog,
-    alt : 'Frogger'
-  },
-  tetris : {
-    href : "https://s-mehnert.github.io/tetris_game/",
-    src : tetris,
-    alt : "Tetris"
-  }
-};
+import {games} from './gameData';
 
 function main() {
   
@@ -53,8 +9,8 @@ function main() {
   // need to include the following attribute in img: onClick={handleClick}
 
   let figures = [];
-  for (const game in thumbnails) {
-    figures.push({id: game, src: thumbnails[game].src, alt: thumbnails[game].alt, href: thumbnails[game].href});
+  for (const game in games) {
+    figures.push({id: game, src: games[game].src, alt: games[game].alt, href: games[game].href});
   }
 
   figures = figures.map(game => (
