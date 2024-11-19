57ef35df6ceff4b0673f015fae471aa547ea6a05
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-11-19 10:29:22 +0100

    add direct links to games' webpages, adjus styling for small screens

--- src/components/gameBox.js
+++ src/components/gameBox.js
@@ -3,8 +3,8 @@ import React from "react";
 function GameBox(props) {
     return (
         <>
-            <h2>{props.game}</h2>
-            <iframe className="game-window" id={props.game} src={props.src}></iframe>
+            <h2>Play <a href={props.src} target="_blank">{props.game}</a> on original page</h2>
+            <iframe allow="fullscreen" className="game-window" id={props.game} src={props.src}></iframe>
         </>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,8 @@ import GameIcon from './gameIcon';
 
 function Header(props) {
   return (
-    <div>
-      <div className="games">
+    <div className="games">
         {props.data.map(game => <GameIcon name={game.name} href={game.href} src={game.src} alt={game.alt} key={game.id} handler={props.handler} />)}
-      </div>
     </div>
   );
 }
